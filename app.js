22e69942fd0ffa69f77991d8909c435689bbd920
N@yan3x_H4ck3r <holanda.nayana@gmail.com>
2018-05-04 04:38:17 -0300

    make it better

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 require('mongoose-type-email');
-mongoose.connect('mongodb://localhost:27017/invester');
+mongoose.connect('mongodb://heroku_zg1dkqqt:vvm3t52m8rgkt9aa4d13fqa5qf@ds113620-a0.mlab.com:13620,ds113620-a1.mlab.com:13620/heroku_zg1dkqqt?replicaSet=rs-ds113620/invester');
 
 var app = express();
 
